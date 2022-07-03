b3f744fe308fc1fd4e0afc0c80b61ddb42035349
Jovan <jovandamjanovic@gmail.com>
2022-07-03 18:41:51 +0200

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    extends: ['plugin:@typescript-eslint/recommended'],
+    env: {
+        node: true,
+    },
+    rules: {
+        quotes: ['warn', 'single']
+    }
+};
